facdd62c57db3298236dd819cd0c657fb22098d2
Koby Levy <86796339+KobyLevy512@users.noreply.github.com>
2023-03-13 10:56:09 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Reverse(string s)
+        {
+            if (s.Length == 0) return;
+            Reverse(s.Substring(1));
+            Console.Write(s[0]);
+        }
+        static void Main(string[] args)
+        {
+            Reverse("abcd");
+            Console.ReadLine();
+        }
+    }
+}
